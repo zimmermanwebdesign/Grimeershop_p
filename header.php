193ef7bd75f0d4f8a661c26de67b21f97eaa9c37
Chris Zimmerman <chris@zimmermanwebdesign.be>
2019-09-07 13:24:25 +0200

    all files first time

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="nl-BE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<p>Hallo WordPress</p>
+<?php get_footer(); ?>
